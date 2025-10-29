81239bb934438300c48c93cba3e38bb8cae5e253
gii002 <gchimwaza5@gmail.com>
2025-10-29 06:20:57 +0200

    Update Login.js

--- clearance/src/pages/Login.js
+++ clearance/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
-import image from "../public/image.jpg"; // ✅ Make sure your logo path is correct
+import image from "./image.jpg"; // ✅ Make sure your logo path is correct
 
 export default function Login() {
   const [role, setRole] = useState("student");
